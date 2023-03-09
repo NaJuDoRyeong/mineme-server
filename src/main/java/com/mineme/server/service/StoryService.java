f4c7a310be59886a0fd70415c719443b8d69897f
jujuwon <webmon@kakao.com>
2023-03-10 00:34:32 +0900

    [MIN-50] feat: 스토리 서비스 로직 구현 (#15)

--- src/main/java/com/mineme/server/service/StoryService.java
+++ src/main/java/com/mineme/server/service/StoryService.java
@@ -1,11 +1,20 @@
 package com.mineme.server.service;
 
-import org.springframework.stereotype.Service;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.mineme.server.common.enums.ErrorCode;
+import com.mineme.server.common.exception.CustomException;
 import com.mineme.server.dto.Story.Detail;
 import com.mineme.server.dto.Story.SaveRequest;
 import com.mineme.server.dto.Story.Stories;
+import com.mineme.server.entity.Post;
 import com.mineme.server.entity.User;
+import com.mineme.server.entity.enums.UserState;
 import com.mineme.server.repository.PostRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -14,30 +23,49 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @Slf4j
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class StoryService {
+	private final AuthService authService;
 	private final PostRepository postRepository;
 
 	public Stories getStories() {
-		// TODO jwt 이용 현재 유저 가져오기
-		User currentUser = getCurrentUser();
-		// TODO Paging 이용해서 목록 제공하기
+		// TODO 현재 유저가 커플인지 검증 (ACTIVATED)
 
-		return null;
+		User currentUser = authService.getCurrentUser();
+
+		LocalDate start = LocalDate.now().with(TemporalAdjusters.firstDayOfMonth());
+		LocalDate end = LocalDate.now().with(TemporalAdjusters.lastDayOfMonth());
+		// TODO Paging 이용해서 목록 제공하기
+		List<Post> posts = postRepository.findMonthlyPosts(currentUser, start, end);
+
+		return Stories.of(start, posts);
 	}
 
 	public Detail getStory(Long storyId) {
-		// TODO jwt 이용 현재 유저 가져오기
 		// TODO Paging 이용해서 목록 제공하기
-		return new Detail();
+		return Detail.of(getPostAndValidate(storyId));
 	}
 
+	@Transactional
 	public void addStory(SaveRequest request) {
-		// TODO jwt 이용 현재 유저 가져오기
-		postRepository.save(request.toEntity());
+		// TODO 현재 유저가 커플인지 검증 (ACTIVATED?).
+		postRepository.save(request.toEntity(authService.getCurrentUser()));
 	}
 
-	// TODO test용
-	private User getCurrentUser() {
-		return null;
+	public Post getPostAndValidate(Long postId) {
+		Post post = postRepository.findById(postId)
+			.orElseThrow(() -> new CustomException(ErrorCode.INVALID_REQUEST));
+
+		User user = authService.getCurrentUser();
+
+		if (post.getUserId().getId() != user.getId()) {
+			throw new CustomException(ErrorCode.INVALID_USER);
+		}
+		// TODO AuthService 에 유저 유효성 로직 구현되면 변경
+		// if (user.getUserState() != UserState.ACTIVATED) {
+		// 	throw new CustomException(ErrorCode.INVALID_USER);
+		// }
+
+		return post;
 	}
 }
