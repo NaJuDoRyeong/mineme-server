80b04c04c6a290fce53abcec8ac62374fe99b098
jujuwon <webmon@kakao.com>
2023-03-10 00:33:55 +0900

    [MIN-50] feat: 월별 스토리 목록 조회 메소드 추가 (#15)

--- src/main/java/com/mineme/server/repository/PostRepository.java
+++ src/main/java/com/mineme/server/repository/PostRepository.java
@@ -1,9 +1,17 @@
 package com.mineme.server.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.mineme.server.entity.Post;
+import com.mineme.server.entity.User;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
+	@Query("select p from Post p join fetch p.userId where p.datedAt between :start and :end and p.userId = :user")
+	List<Post> findMonthlyPosts(@Param("user") User currentUser, @Param("start") LocalDate start, @Param("end") LocalDate end);
 }
