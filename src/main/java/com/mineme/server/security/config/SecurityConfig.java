9e9af1ef0b59e12616f3516973bcf44e782ed90d
Floodnut <gsniper777@gmail.com>
2023-05-08 20:39:51 +0900

    [MIN-72] fix: 중복 FilterChain 접근으로 잘못된 응답이 발생하는 문제를 수정 (#37)

--- src/main/java/com/mineme/server/security/config/SecurityConfig.java
+++ src/main/java/com/mineme/server/security/config/SecurityConfig.java
@@ -13,7 +13,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.mineme.server.security.filter.JwtAuthenticationFilter;
-import com.mineme.server.security.filter.JwtExceptionFilter;
 import com.mineme.server.security.filter.JwtGlobalEntryPoint;
 import com.mineme.server.security.provider.JwtTokenProvider;
 import com.mineme.server.security.provider.UserJwtAuthenticationProvider;
@@ -26,7 +25,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	private final CustomUserDetailsService userDetailsService;
 	private final JwtGlobalEntryPoint jwtGlobalEntryPoint;
-	private final JwtExceptionFilter jwtExceptionFilter;
 	private final JwtTokenProvider jwtTokenProvider;
 	private final Properties properties;
 
@@ -51,6 +49,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.and()
 			.authorizeRequests()
 			.antMatchers("/api/v1/auth/**").permitAll()
+			.antMatchers("/kakao").permitAll()
 			.and()
 			.authorizeRequests()
 			.anyRequest().authenticated()
@@ -58,7 +57,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.exceptionHandling().authenticationEntryPoint(jwtGlobalEntryPoint)
 			.and()
 			.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider, properties),
-				UsernamePasswordAuthenticationFilter.class)
-			.addFilterBefore(jwtExceptionFilter, JwtAuthenticationFilter.class);
+				UsernamePasswordAuthenticationFilter.class);
 	}
 }
